0d1e08942a21094e21ca2100f814339f2bface01
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-03-10 16:23:05 +0530

    Add Maps Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import LandingPageBody from '../src/components/LandingPageBody';
+// import LandingPageBody from '../src/components/LandingPageBody';
 // import RoomsDropdownField from './components/RoomsDropdownField';
 
-import ListingPage from './components/ListingPage';
+// import ListingPage from './components/ListingPage';
+import MyMapComponent from './ReactGoogleMaps';
+
+// { /* <div className="App" >
+//         <LandingPageBody />
+//         <ListingPage />
+//       </div> */ }
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        <LandingPageBody />
-        <ListingPage />
-      </div>
+      <MyMapComponent isMarkerShown />
     );
   }
 }
