c908e1e927bb4e37ce4ea69949532652dafff80d
AbhilashJN <abhilashjn107@gmail.com>
2018-03-21 14:51:47 +0530

    Added manage bookings page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ListingPage from '../src/components/ListingPage';
 // import { changeLoginState } from './redux/actions';
 // import Login from './components/Login';
 import PaymentPage from './components/PaymentPage';
+import ManageBookingsPage from './components/ManageBookingsPage';
 
 class App extends Component {
   // componentDidMount() {
@@ -25,6 +26,7 @@ class App extends Component {
           {/* <Route path="/signUp" component={SignUpPage} />
           <Route path="/signIn" component={Login} /> */}
           <Route path="/payment" component={PaymentPage} />
+          <Route path="/manageBookings" component={ManageBookingsPage} />
         </Switch>
       </div>
     );
---
+++ src/components/ManageBookingsPage/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ManageBookingsPage.css';
+import ManageBookingsRow from '../ManageBookingsRow';
+
+class ManageBookingsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBookings: [],
+    };
+  }
+  componentDidMount() {
+    const jwt = window.localStorage.getItem('token');
+    const options = {
+      method: 'get',
+      headers: {
+        authorization: jwt,
+      },
+    };
+    fetch('/userViewBookings', options)
+      .then(response => response.json())
+      .then((respJSON) => {
+        this.setState({ allBookings: respJSON });
+      });
+  }
+  render() {
+    const allBookingRows = [];
+    this.state.allBookings.forEach((booking) => {
+      const bookingRow = (<ManageBookingsRow
+        key={booking.bookingid}
+        amount={booking.amount}
+        bookingDate={booking.bookingdate}
+        bookingId={booking.bookingid}
+        checkIn={booking.checkin}
+        checkOut={booking.checkout}
+        hotelName={booking.hotelname}
+        numOfGuests={booking.numofguests}
+        numOfRooms={booking.numofrooms}
+      />);
+      allBookingRows.push(bookingRow);
+    });
+    return (
+      <div>
+        {allBookingRows}
+      </div>
+    );
+  }
+}
+ManageBookingsPage.defaultProps = {
+};
+ManageBookingsPage.propTypes = {
+};
+export default ManageBookingsPage;
