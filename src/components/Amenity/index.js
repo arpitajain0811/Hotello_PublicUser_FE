baff0b905aa22231bd07d72ad6d03f53bb89b2be
aakash_raj <aakash.raj5@gmail.com>
2018-03-26 12:19:16 +0530

    added some icons

--- src/components/Amenity/index.js
+++ src/components/Amenity/index.js
@@ -13,7 +13,7 @@ const Amenity = (props) => {
     imgSrc = '../../images/icons/cafe.svg';
   } else if (props.text.includes('carte')) {
     imgSrc = '../../images/icons/alacarte.svg';
-  } else if (props.text.includes('Maestro')) {
+  } else if ((props.text.includes('Maestro')) || (props.text.includes('JCB Card')) || (props.text.includes('American Expres'))) {
     imgSrc = '../../images/icons/card.svg';
   } else if (props.text === 'Air conditioned') {
     imgSrc = '../../images/icons/ac.svg';
@@ -21,8 +21,20 @@ const Amenity = (props) => {
     imgSrc = '../../images/icons/buffet.svg';
   } else if (props.text === 'Airport') {
     imgSrc = '../../images/icons/airport.svg';
-  } else if (props.text === ('Hi-fi')) {
+  } else if ((props.text === ('Hi-fi')) || (props.text === ('Wireless Internet Access')) || (props.text === ('WiFi everywhere'))) {
     imgSrc = '../../images/icons/wifi.svg';
+  } else if ((props.text === ('Bathtub or shower')) || (props.text === ('Shared Bathroom'))) {
+    imgSrc = '../../images/icons/bathroom.svg';
+  } else if (props.text === ('24 Hour Front Desk')) {
+    imgSrc = '../../images/icons/frontDesk.svg';
+  } else if (props.text === ('Bar / Lounge')) {
+    imgSrc = '../../images/icons/bar.svg';
+  } else if (props.text === ('Computer station')) {
+    imgSrc = '../../images/icons/computer.svg';
+  } else if (props.text === ('Meeting Rooms')) {
+    imgSrc = '../../images/icons/conference.svg';
+  } else if (props.text === ('Meeting Rooms')) {
+    imgSrc = '../../images/icons/conference.svg';
   } else {
     imgSrc = '../../images/icons/general.svg';
   }
